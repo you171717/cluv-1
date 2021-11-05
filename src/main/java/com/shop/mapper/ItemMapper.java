81ed0a282befbf8fe4de13f33a663f37040c5549
DESKTOP-AE8EH9T\YHJ <you171717@gsitm.com>
2021-11-05 10:49:29 +0900

    Add Chapter 1 and 2 Source Codes

---
+++ src/main/java/com/shop/mapper/ItemMapper.java
@@ -0,0 +1,28 @@
+package com.shop.mapper;
+
+import com.shop.dto.ItemDto;
+import com.shop.entity.Item;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ItemMapper {
+
+    void insertItem(ItemDto itemDto);
+
+    List<ItemDto> findByItemNm(String itemNm);
+
+    List<ItemDto> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<ItemDto> findByPriceLessThan(Integer price);
+
+    List<ItemDto> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    List<ItemDto> findByItemDetail(String itemDetail);
+
+    List<ItemDto> findByItemDetailByNative(String itemDetail);
+
+}
