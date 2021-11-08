1845b3b338b8728dc5e2faa96e887e5abed52518
OhSeungyeon <s2018w10@e-mirim.hs.kr>
2021-11-08 22:22:10 +0900

    chapter 5

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity {
 
     @Id
     @Column(name="item_id")
@@ -35,9 +35,5 @@ public class Item {
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
-
-    private LocalDateTime regTime;
-
-    private LocalDateTime updateTime;
 
 }
