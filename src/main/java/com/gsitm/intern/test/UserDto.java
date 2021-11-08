14ba8d6ff978910c678dab99163ab60bda9a087c
munyeeun <yeeun@gsitm.com>
2021-11-08 12:39:26 +0900

    'test'

---
+++ src/main/java/com/gsitm/intern/test/UserDto.java
@@ -0,0 +1,15 @@
+package com.gsitm.intern.test;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+
+    private String name;
+    private Integer age;
+
+}
