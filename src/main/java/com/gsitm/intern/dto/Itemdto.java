fe6e0119f2cd0e29c02e27af70743dfaf288e5f1
DESKTOP-OET01PG\bgb75 <it1668@gsitm.com>
2021-11-08 13:06:17 +0900

    test2

---
+++ src/main/java/com/gsitm/intern/dto/Itemdto.java
@@ -0,0 +1,26 @@
+package com.gsitm.intern.dto;
+
+import com.gsitm.intern.constant.ItemSellStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Itemdto {
+
+    private Long id;//상품 코드
+
+    private String itemNm;//상품명
+
+    private int price;//가격
+
+    private int stockNumber;//재고수량
+
+    private String itemDetail;//상품 상세 설명
+
+    private ItemSellStatus itemSellStatus;//상품 판매 상태
+
+    private LocalDateTime regTime;//등록 시간
+
+    private LocalDateTime updateTime;//수정 시간
+}
---
+++ src/main/java/com/gsitm/intern/model.java
@@ -0,0 +1,4 @@
+package com.gsitm.intern;
+
+public class model {
+}
