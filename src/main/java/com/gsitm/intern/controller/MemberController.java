48a1b07d077448b0343f95aa938a8355d55a7846
DESKTOP-OET01PG\bgb75 <it1668@gsitm.com>
2021-11-09 23:40:15 +0900

    Chap04 Security

--- src/main/java/com/gsitm/intern/controller/MemberController.java
+++ src/main/java/com/gsitm/intern/controller/MemberController.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.gsitm.intern.entity.Member;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -14,10 +20,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm"; // value 객체를 name 이름으로 추가한다. 뷰 코드에서는 name으로 지정한 이름을 통해서 value를 사용한다.
     }
+
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
+
+
 }
