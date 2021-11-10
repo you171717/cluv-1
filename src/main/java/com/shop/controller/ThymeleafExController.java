bb4d5d23acdd0eb6d651837e349ebf5a838a2dfa
DESKTOP-4SMQK8I\yh <yunha212@gsitm.com>
2021-11-11 08:45:53 +0900

    add chap03

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -74,4 +74,18 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx05";
     }
 
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model){
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+
+    }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07(){
+        return "thymeleafEx/thymeleafEx07";
+    }
+
+
 }
