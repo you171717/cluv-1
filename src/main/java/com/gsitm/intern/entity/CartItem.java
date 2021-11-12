20be550c5fc8966f0629e2014ab1fe24f80ab2c5
SSSSOy <93689870+SSSSOy@users.noreply.github.com>
2021-11-12 17:19:34 +0900

    Chapter.5, 6
    Chapter.5 : 연관 관계 매핑
    Chapter.6 : 상품 등록 및 조회하기

---
+++ src/main/java/com/gsitm/intern/entity/CartItem.java
@@ -0,0 +1,31 @@
+package com.gsitm.intern.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter @Setter
+@Table(name = "cart_item")
+@ToString
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+
+}
---
+++ src/main/java/com/gsitm/intern/repository/ItemRepositoryCustom.java
@@ -0,0 +1,15 @@
+package com.gsitm.intern.repository;
+
+import com.gsitm.intern.dto.ItemSearchDto;
+import com.gsitm.intern.dto.MainItemDto;
+import com.gsitm.intern.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ItemRepositoryCustom {
+
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+}
