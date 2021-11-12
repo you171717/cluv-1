6f54bac94cc05369355a56d386bb68d1cf2ece5d
DESKTOP-HO05CE8\CWJ <wjcho@gsitm.com>
2021-11-12 17:44:57 +0900

    feat: Add CH.04 Spring Security complete
    쇼핑몰 프로젝트 교재 4장 스프링 시큐리티 코드 구현완료

--- src/main/java/com/gsitm/intern/config/SecurityConfig.java
+++ src/main/java/com/gsitm/intern/config/SecurityConfig.java
@@ -1,22 +1,58 @@
 package com.gsitm.intern.config;
 
+import com.gsitm.intern.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity  //어노테이션 통해 SpringSecurityFilterChain 자동으로 포함
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
 
+        http.authorizeRequests()
+                .mvcMatchers("/","/members/**",
+                        "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.exceptionHandling()    //인증되지 않은 사용자 리소스에 접근했을 때 핸들러 등록
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
     }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**","/js/**","/img/**");
+    }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){   //BCryptPasswordEncoder의 해시 함수 이용해서 암호화 저장
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService)  //userDetailService를 구현하고 있는 객체로 memberService 지정
+                .passwordEncoder(passwordEncoder());
     }
 }
---
+++ src/test/java/com/gsitm/intern/controller/MemberControllerTest.java
@@ -0,0 +1,66 @@
+package com.gsitm.intern.controller;
+
+import com.gsitm.intern.dto.MemberFormDto;
+import com.gsitm.intern.entity.Member;
+import com.gsitm.intern.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc   //MockMvc 테스트
+@Transactional
+@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yml" })
+public class MemberControllerTest {
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;    //실제 객체와 비슷하지만 테스트에 필요한 기능만 가지는 가짜 개체
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword(password);
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception{
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());  //로그인 성공하여 인증되면 테스트 코드 통과
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailTest() throws Exception{
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+}
