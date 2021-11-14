74d374acf5353eb688fed336259dd3cd7f1f03f5
Aproid <you171717@naver.com>
2021-11-14 13:46:04 +0900

    Add Chapter 5 Source Codes

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -7,13 +7,14 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "item")
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity {
 
     @Id
     @Column(name = "item_id")
@@ -39,5 +40,15 @@ public class Item {
     private LocalDateTime regTime; // 등록 시간
 
     private LocalDateTime updateTime; // 수정 시간
+
+    /*
+    @ManyToMany
+    @JoinTable(
+        name = "member_item",
+        joinColumns = @JoinColumn(name = "member_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Member> member;
+    */
 
 }
