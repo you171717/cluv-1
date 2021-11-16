1229697e3a79f2aa84aecef6f14d8b115dcdfbd8
yeonee <yeon@gsitm.com>
2021-11-17 07:03:40 +0900

    company

---
+++ src/main/java/com/gsitm/intern/shop/repository/ItemImgRepository.java
@@ -0,0 +1,12 @@
+package com.gsitm.intern.shop.repository;
+
+import com.gsitm.intern.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+}
---
+++ src/main/java/com/gsitm/intern/shop/service/ItemService.java
@@ -0,0 +1,43 @@
+package com.gsitm.intern.shop.service;
+
+import com.gsitm.intern.shop.dto.ItemFormDto;
+import com.gsitm.intern.shop.entity.Item;
+import com.gsitm.intern.shop.entity.ItemImg;
+import com.gsitm.intern.shop.repository.ItemImgRepository;
+import com.gsitm.intern.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++) {
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+            if(i == 0)
+                itemImg.setRepimgYn("Y");
+            else
+                itemImg.setRepimgYn("N");
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+}
