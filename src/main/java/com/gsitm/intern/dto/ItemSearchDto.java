ffc34d4f97865f962f1714f0f8a4a8639a4735e5
munyeeun <yeeun@gsitm.com>
2021-11-17 15:59:31 +0900

    'chapter6'

---
+++ src/main/java/com/gsitm/intern/dto/ItemSearchDto.java
@@ -0,0 +1,17 @@
+package com.gsitm.intern.dto;
+
+import com.gsitm.intern.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ItemSearchDto {
+
+    private String searchDateType;
+
+    private ItemSellStatus searchSellStatus;
+
+    private String searchBy;
+
+    private String searchQuery = "";
+}
