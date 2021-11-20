5d255336e95c9b90f4b926cb95ac761adcfe35cb
munyeeun <yeeun@gsitm.com>
2021-11-20 16:31:30 +0900

    'chapter8'

--- src/main/java/com/gsitm/intern/dto/OrderDto.java
+++ src/main/java/com/gsitm/intern/dto/OrderDto.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotNull;
 public class OrderDto {
 
     @NotNull(message = "상품 아이디는 필수 입력 값입니다.")
-    private long itemId;
+    private Long itemId;
 
     @Min(value = 1, message = "최소 주문 수량은 1개 입니다.")
     @Max(value = 999, message = "최대 주문 수량은 999개 입니다.")
--- src/main/java/com/gsitm/intern/entity/CartItem.java
+++ src/main/java/com/gsitm/intern/entity/CartItem.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 @Getter @Setter
 @Table(name = "cart_item")
-public class CartItem {
+public class CartItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -23,4 +23,23 @@ public class CartItem {
     private Item item;
 
     private int count;
+
+    public static CartItem createCartItem(Cart cart, Item item, int count){
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+    }
+
+    public void addCount(int count){
+        this.count += count;
+    }
+
+    public void updateCount(int count){
+        this.count = count;
+    }
+
+
 }
+
