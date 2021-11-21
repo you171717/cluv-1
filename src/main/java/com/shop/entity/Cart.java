c6e0a77e84d88abe71fc1b5fee05d807a03cee23
woojin990219 <woojin990219@gsitm.com>
2021-11-22 00:26:11 +0900

    chapter 8.1 + 8.2

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -23,5 +23,10 @@ public class Cart extends BaseEntity{
     @JoinColumn(name="member_id")
     private Member member;
 
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 
 }
